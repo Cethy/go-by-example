507b74d5e1762dc7b476089fa4299f6751214de1
Charles <cethy.hnc@gmail.com>
2024-09-28 22:04:34 +0100

    UPDATED: refactor http-proxy-ssl to optionally listen to https

--- http-proxy-ssl/main.go
+++ http-proxy-ssl/main.go
@@ -9,6 +9,32 @@ import (
 	"net/http"
 )
 
+func listenHttp(urlMode bool, port int) {
+	httpServer := http.NewServeMux()
+	httpServer.HandleFunc("/", httpmiddleware.Chain(httpproxy.GetProxyHandler(urlMode), httpmiddleware.LoggingPre("[HTTP ]"), httpmiddleware.PostLogging()))
+
+	fmt.Println("http listening on port:", port)
+
+	err := http.ListenAndServe(":"+fmt.Sprint(port), httpServer)
+	if err != nil {
+		log.Fatal("[http ]", err)
+		return
+	}
+}
+
+func listenHttps(urlMode bool, port int, certFile string, keyFile string) {
+	httpsServer := http.NewServeMux()
+	httpsServer.HandleFunc("/", httpmiddleware.Chain(httpproxy.GetProxyHandler(urlMode), httpmiddleware.LoggingPre("[HTTPS]"), httpmiddleware.PostLogging()))
+
+	fmt.Println("https listening on port:", port)
+
+	err := http.ListenAndServeTLS(":"+fmt.Sprint(port), certFile, keyFile, httpsServer)
+	if err != nil {
+		log.Fatal("[https]", err)
+		return
+	}
+}
+
 func main() {
 	certFile := flag.String("cert", "", "Certificate file path")
 	keyFile := flag.String("key", "", "Private key file path")
@@ -18,29 +44,8 @@ func main() {
 	urlMode := flag.Bool("url", false, "tells the executable how the proxy should read the target url ; false for default proxy mode, true for url mode : 'http://localhost:8003/?url=https://example.com'")
 	flag.Parse()
 
-	if *certFile == "" || *keyFile == "" {
-		log.Fatal("cert and key params are mandatory")
+	if *certFile != "" && *keyFile != "" {
+		go listenHttps(*urlMode, *portSSL, *certFile, *keyFile)
 	}
-
-	httpServer := http.NewServeMux()
-	httpServer.HandleFunc("/", httpmiddleware.Chain(httpproxy.GetProxyHandler(*urlMode), httpmiddleware.LoggingPre("[HTTP ]"), httpmiddleware.PostLogging()))
-
-	httpsServer := http.NewServeMux()
-	httpsServer.HandleFunc("/", httpmiddleware.Chain(httpproxy.GetProxyHandler(*urlMode), httpmiddleware.LoggingPre("[HTTPS]"), httpmiddleware.PostLogging()))
-
-	fmt.Println("http listening on port:", *port)
-	fmt.Println("https listening on port:", *portSSL)
-
-	go func() {
-		err := http.ListenAndServe(":"+fmt.Sprint(*port), httpServer)
-		if err != nil {
-			log.Fatal("[http]", err)
-			return
-		}
-	}()
-	err := http.ListenAndServeTLS(":"+fmt.Sprint(*portSSL), *certFile, *keyFile, httpsServer)
-	if err != nil {
-		log.Fatal("[https]", err)
-		return
-	}
+	listenHttp(*urlMode, *port)
 }

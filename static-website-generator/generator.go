3124578cc2718d517bad2e52f85c5d8eae0665b1
Charles <cethy.hnc@gmail.com>
2024-09-29 22:09:32 +0100

    UPDATED: Add "fragment" support to static-website-generator

--- static-website-generator/generator.go
+++ static-website-generator/generator.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"slices"
+	"strings"
 )
 
 var processableExtensions = []string{".html"}
@@ -36,12 +38,54 @@ func copyFile(srcPath, targetPath string) {
 	log.Println("copied", srcPath, "to", targetPath)
 }
 
+func removeExt(filename string) string {
+	base := filepath.Base(filename)
+	ext := filepath.Ext(base)
+	return strings.TrimSuffix(base, ext)
+}
+
+func isFragment(filename string) bool {
+	runeSlice := []rune(filename)
+	return runeSlice[0] == '{' && runeSlice[len(runeSlice)-1] == '}'
+}
+
+var fragments map[string]string = make(map[string]string)
+
+func getFragment(fragmentId string) string {
+	if fragments[fragmentId] == "" {
+		readFile, err := os.ReadFile(fragmentId)
+		if err != nil {
+			panic(err)
+		}
+
+		fragments[fragmentId] = string(readFile)
+	}
+	return fragments[fragmentId]
+}
+
 func buildFile(srcPath, targetPath string) {
+	// ignore if it's a fragment
+	filename := removeExt(srcPath)
+	if isFragment(filename) {
+		return
+	}
+
 	readFile, err := os.ReadFile(srcPath)
 	if err != nil {
 		panic(err)
 	}
-	err = os.WriteFile(targetPath, readFile, 0644) // @todo check params
+	fileContent := string(readFile)
+
+	// insert fragments
+	pattern := "(\\{[A-z]+\\})"
+	r, _ := regexp.Compile(pattern)
+	requiredFragments := r.FindAllString(fileContent, -1)
+	for _, fragmentId := range requiredFragments {
+		fragment := getFragment(filepath.Join(filepath.Dir(srcPath), fragmentId+".html"))
+		fileContent = strings.ReplaceAll(fileContent, fragmentId, fragment)
+	}
+
+	err = os.WriteFile(targetPath, []byte(fileContent), 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -59,32 +103,35 @@ func processFile(srcPath, targetPath string) {
 	buildFile(srcPath, targetPath)
 }
 
-func processDir(dirPath, inputPath, outputPath string) {
-	dir, err := os.ReadDir(filepath.Join(inputPath, dirPath))
+func processDir(dirPath string) {
+	dir, err := os.ReadDir(filepath.Join(srcDir, dirPath))
 	if err != nil {
 		panic(err)
 	}
 
-	err = os.MkdirAll(filepath.Join(outputPath, dirPath), 0755) // @todo check perms
+	err = os.MkdirAll(filepath.Join(outputDir, dirPath), 0755) // @todo check perms
 	if err != nil {
 		panic(err)
 	}
 
 	for _, file := range dir {
 		if file.IsDir() {
-			processDir(filepath.Join(dirPath, file.Name()), inputPath, outputPath)
+			processDir(filepath.Join(dirPath, file.Name()))
 			continue
 		}
-		srcPath := filepath.Join(inputPath, dirPath, file.Name())
-		targetPath := filepath.Join(outputPath, dirPath, file.Name())
+		srcPath := filepath.Join(srcDir, dirPath, file.Name())
+		targetPath := filepath.Join(outputDir, dirPath, file.Name())
 
 		processFile(srcPath, targetPath)
 	}
 }
 
+var outputDir string
+var srcDir string
+
 func build(srcFilePathname string) {
-	outputDir := filepath.Join(srcFilePathname, "./output")
-	srcDir := filepath.Join(srcFilePathname, "./html")
+	outputDir = filepath.Join(srcFilePathname, "./output")
+	srcDir = filepath.Join(srcFilePathname, "./html")
 
 	// clean output directory
 	err := os.RemoveAll(outputDir)
@@ -98,7 +145,7 @@ func build(srcFilePathname string) {
 		panic(err)
 	}
 
-	processDir(".", srcDir, outputDir)
+	processDir(".")
 }
 
 func main() {

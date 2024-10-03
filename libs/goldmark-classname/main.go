4324534eff4c0c27a167ad09fa16f5d5b57ce213
Charles <cethy.hnc@gmail.com>
2024-10-03 16:48:50 +0100

    ADDED: custom goldmark plugins (classname injector & tailwindcss processor)

---
+++ libs/goldmark-classname/main.go
@@ -0,0 +1,28 @@
+package goldmark_classname
+
+import (
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/text"
+)
+
+type Config struct {
+	ClassNameProvider func(node ast.Node) string
+}
+
+type ClassNameTransformer struct {
+	Config Config
+}
+
+func (c *ClassNameTransformer) Transform(node *ast.Document, reader text.Reader, pc parser.Context) {
+	ast.Walk(node, func(n ast.Node, entering bool) (status ast.WalkStatus, err error) {
+		if entering {
+			n.SetAttributeString("class", c.Config.ClassNameProvider(n))
+		}
+		return ast.WalkContinue, nil
+	})
+}
+
+func NewTransformer(config Config) parser.ASTTransformer {
+	return &ClassNameTransformer{Config: config}
+}
